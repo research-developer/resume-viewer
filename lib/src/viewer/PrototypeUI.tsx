4bb01249a166046e8ab88e3f817d8b7fc49ca75f
Richard Adleta <radleta@gmail.com>
2025-05-07 17:00:49 -0500

    Refactor PrototypeUI component for improved positioning and z-index

--- lib/src/viewer/PrototypeUI.tsx
+++ lib/src/viewer/PrototypeUI.tsx
@@ -6,12 +6,14 @@ import { FC } from "react";
  * Ribbon is non-interactive and responsive. Follows project UI standards.
  */
 export const PrototypeUI: FC = () => (
-  <div
-    className="select-none pointer-events-none z-50 absolute top-7 md:top-14 right-[-55px] md:right-[-55px] w-[185px] md:w-[250px] overflow-hidden"
-    style={{ transform: "rotate(45deg)" }}
-  >
-    <span className="block text-center bg-accent-yellow-dark text-accent-yellow-light font-semibold tracking-widest text-xs md:text-xl py-1 shadow-sm border border-accent-dark uppercase">
-      PROTOTYPE
-    </span>
+  <div className="select-none pointer-events-none z-[9999] absolute top-0 md:top-0 right-0 overflow-hidden h-[175px]">
+    <div
+      className="relative top-7 md:top-12 w-[185px] md:w-[250px] right-[-55px] md:right-[-55px]"
+      style={{ transform: "rotate(45deg)" }}
+    >
+      <span className="block text-center bg-accent-yellow-dark text-accent-yellow-light font-semibold tracking-widest text-xs md:text-xl py-1 shadow-sm border border-accent-dark uppercase">
+        PROTOTYPE
+      </span>
+    </div>
   </div>
 );
